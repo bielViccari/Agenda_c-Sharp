b19f99636714a7e9e21cd37b0caf36af8523a597
Gabriel Viccari <87938998+bielViccari@users.noreply.github.com>
2022-09-27 10:42:14 -0300

    First commit

---
+++ Agenda2022/Contato/frmAgenda.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Contato
+{
+    partial class frmAgenda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAgenda));
+            this.lstAgenda = new System.Windows.Forms.ListBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtFone = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnRestaurar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.cmbSexo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstAgenda
+            // 
+            this.lstAgenda.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstAgenda.FormattingEnabled = true;
+            this.lstAgenda.ItemHeight = 25;
+            this.lstAgenda.Location = new System.Drawing.Point(347, 85);
+            this.lstAgenda.Name = "lstAgenda";
+            this.lstAgenda.Size = new System.Drawing.Size(349, 179);
+            this.lstAgenda.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNome.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(20, 96);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(321, 28);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtFone
+            // 
+            this.txtFone.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFone.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFone.Location = new System.Drawing.Point(20, 150);
+            this.txtFone.Name = "txtFone";
+            this.txtFone.Size = new System.Drawing.Size(321, 28);
+            this.txtFone.TabIndex = 1;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnAdicionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdicionar.Location = new System.Drawing.Point(20, 246);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(158, 44);
+            this.btnAdicionar.TabIndex = 3;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnRestaurar
+            // 
+            this.btnRestaurar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnRestaurar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestaurar.Location = new System.Drawing.Point(183, 246);
+            this.btnRestaurar.Name = "btnRestaurar";
+            this.btnRestaurar.Size = new System.Drawing.Size(158, 44);
+            this.btnRestaurar.TabIndex = 4;
+            this.btnRestaurar.Text = "Restaurar";
+            this.btnRestaurar.UseVisualStyleBackColor = true;
+            this.btnRestaurar.Click += new System.EventHandler(this.btnRestaurar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fone";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Location = new System.Drawing.Point(581, 294);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(110, 44);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar Agenda";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // cmbSexo
+            // 
+            this.cmbSexo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSexo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbSexo.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSexo.FormattingEnabled = true;
+            this.cmbSexo.Items.AddRange(new object[] {
+            "",
+            "Masculino",
+            "Feminino"});
+            this.cmbSexo.Location = new System.Drawing.Point(20, 203);
+            this.cmbSexo.Name = "cmbSexo";
+            this.cmbSexo.Size = new System.Drawing.Size(321, 33);
+            this.cmbSexo.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Sexo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(418, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 42);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Agenda 2022";
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.Location = new System.Drawing.Point(183, 294);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(158, 44);
+            this.btnExcluir.TabIndex = 6;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnNovo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNovo.Location = new System.Drawing.Point(20, 294);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(158, 44);
+            this.btnNovo.TabIndex = 5;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnFechar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnFechar.FlatAppearance.BorderSize = 0;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnFechar.Location = new System.Drawing.Point(84, 37);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(82, 23);
+            this.btnFechar.TabIndex = 16;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnMinimizar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnMinimizar.FlatAppearance.BorderSize = 0;
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimizar.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.btnMinimizar.Location = new System.Drawing.Point(12, 37);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(66, 23);
+            this.btnMinimizar.TabIndex = 17;
+            this.btnMinimizar.Text = "Minimizar";
+            this.btnMinimizar.UseVisualStyleBackColor = false;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(710, 74);
+            this.panel1.TabIndex = 18;
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnImportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImportar.Location = new System.Drawing.Point(347, 294);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(110, 44);
+            this.btnImportar.TabIndex = 19;
+            this.btnImportar.Text = "Importar";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnExportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExportar.Location = new System.Drawing.Point(464, 294);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(110, 44);
+            this.btnExportar.TabIndex = 20;
+            this.btnExportar.Text = "Exportar";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // frmAgenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 359);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnImportar);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbSexo);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRestaurar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.txtFone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lstAgenda);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmAgenda";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "_";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstAgenda;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtFone;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnRestaurar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.Button btnExportar;
+    }
+}
+
---
+++ Agenda2022/Contato/frmAgenda.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+//salvar e ler o arquivo
+using System.IO;
+
+namespace Contato
+{
+    public partial class frmAgenda : Form
+    {
+
+        public frmAgenda()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show(
+                "Quer realmente fechar a aplicação?",
+                "Informação",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question, 
+                MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if ((txtNome.Text != "") && (txtFone.Text != "") && (cmbSexo.Text != ""))
+            {
+                lstAgenda.Items.Add(txtNome.Text + "|" + txtFone.Text + "|" + cmbSexo.Text);
+
+                btnNovo.PerformClick();
+
+                //btnNovo_Click(sender, e);
+
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Preencha todos os campos!",
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                txtNome.Focus();
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtFone.Clear();
+            cmbSexo.Text = "";
+            txtNome.Focus();
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            if(lstAgenda.SelectedIndex >= 0)
+            {
+                string[] campos;
+                string selecionado = lstAgenda.SelectedItem.ToString();
+                campos = selecionado.Split('|');
+
+                txtNome.Text = campos[0];
+                txtFone.Text = campos[1];
+                cmbSexo.Text = campos[2];
+
+                lstAgenda.Items.RemoveAt(lstAgenda.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Selecione um registro da Lista!",
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if(lstAgenda.SelectedIndex >= 0)
+            {
+                if(MessageBox.Show(
+                    "Deseja realmente excluir o registro selecionado!", 
+                    "Informação",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                {
+                    lstAgenda.Items.RemoveAt(lstAgenda.SelectedIndex);
+                }
+                else
+                {
+                    MessageBox.Show(
+                        "Ação cancelada pelo usuário!",
+                        "Informação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            if (lstAgenda.Items.Count > 0) //verifica se há contatos na agenda
+            {                
+                FileInfo salvarArquivo = new FileInfo(@".\bk_agenda.txt");
+
+                if (!salvarArquivo.Exists) // verifica o o bk_agenda.txt existe
+                {
+                    using (StreamWriter sa = salvarArquivo.CreateText())
+                    {
+                        for (int i = 0; i < lstAgenda.Items.Count; i++)
+                        {
+                            sa.WriteLine(lstAgenda.Items[i].ToString());
+                        }
+                    }
+                    MessageBox.Show(
+                        "Os dados foram exportados com sucesso!",
+                        "Informação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+                else
+                {
+                    if(MessageBox.Show(
+                        "Deseja sobrepor o arquivo de backup?",
+                        "Informação",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Exclamation)== DialogResult.Yes)
+                    {
+                        using (StreamWriter sa = salvarArquivo.CreateText())
+                        {
+                            for (int i = 0; i < lstAgenda.Items.Count; i++)
+                            {
+                                sa.WriteLine(lstAgenda.Items[i].ToString());
+                            }
+                        }
+                        MessageBox.Show(
+                            "Os dados foram exportados com sucesso!",
+                            "Informação",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(
+                    "A agenda está vazia!",
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+    }
+}
